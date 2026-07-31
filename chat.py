d320a56353425568d824c0c84ebf9c1206ff421f
MarkFrizen <XLy(o2w95HL2@85>
2026-07-31 11:31:40 +0300

    Базовый RAG: чат с PDF / веб-страницей

--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, WebBaseLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
